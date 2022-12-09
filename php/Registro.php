8fa1df28f378e7daa0703d82ec0753be68bacadb
faustogengaalfaro@gmail.com <faustogengaalfaro@gmail.com>
2022-12-09 08:37:04 -0600

    registro funciona con diferentes fechas

---
+++ php/Registro.php
@@ -0,0 +1,100 @@
+<?php
+session_start();
+// database connection code
+include 'connection.php';
+$conn = OpenCon();
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+};
+// get the session records
+$txtNombre = $_SESSION['txtNombre'];
+$txtApellido = $_SESSION['txtApellido'];
+$txtEmail = $_SESSION['txtEmail'];
+$numTelefono = $_SESSION['numTelefono'];
+$numCedula = $_SESSION['numCedula'];
+$numPlaca = $_SESSION['numPlaca'];
+$txtMarca = $_SESSION['txtMarca'];
+$txtColor = $_SESSION['txtColor'];
+$Fecha = $_SESSION['Fecha'];
+$Hora = $_SESSION['Hora'];
+// get post records
+$numEspacio = $_POST['numEspacio'];
+
+// check si existe ya el usuario
+
+$foundID = false;
+$foundplaca = false;
+
+$sqlinsert1 = "INSERT INTO `dbo.persona` (ID_Persona, Nombre, Correo, Telefono) VALUES ('$numCedula', '$txtNombre', '$txtEmail', '$numTelefono')";
+$sqlinsert3 = "INSERT INTO `dbo.vehiculo` (Placa, Marca, ID_Persona, Color) VALUES ('$numPlaca', '$txtMarca', '$numCedula', '$txtColor')";
+$sqlinsert4 = "INSERT INTO `dbo.reservacion` (ID_Persona, Placa, Espacio, Fecha, Hora) VALUES ('$numCedula', '$numPlaca', '$numEspacio', '$Fecha', '$Hora')";
+
+$sqlquery = "SELECT ID_Persona, Nombre FROM `dbo.persona`";
+
+$result = mysqli_query($conn, $sqlquery);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+
+    while($row = mysqli_fetch_assoc($result)) {
+        if ($row["ID_Persona"] == $numCedula && $row["Nombre"] == $txtNombre) {
+
+            //usuario existente
+            $foundID = true;
+
+            //check si existe la placa
+            $sqlquery2 = "SELECT Placa, ID_Persona FROM `dbo.vehiculo` WHERE ID_Persona = '$numCedula'";
+            $result2 = mysqli_query($conn, $sqlquery2);
+
+            while($row2 = mysqli_fetch_assoc($result2)) {
+                
+                if ($row2["Placa"] == $numPlaca) {
+                    //placa existente, insertar reservacion y parqueo
+                    $foundplaca = true;
+
+                    mysqli_query($conn, $sqlinsert4);  
+                }
+            }
+
+            if ($foundplaca == false) {
+                //placa no existente, insertar vehiculo, reservacion y parqueo
+                mysqli_query($conn, $sqlinsert3);
+                mysqli_query($conn, $sqlinsert4);
+            }
+        }
+    }
+    if ($foundID == false) {
+        //Usuario no existente, insertar persona, vehiculo, reservacion y parqueo
+        if ($conn->query($sqlinsert1) === TRUE) {
+            echo "reservation";
+          } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+          }
+          if ($conn->query($sqlinsert3) === TRUE) {
+            echo "vehiculo";
+          } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+          }
+          if ($conn->query($sqlinsert4) === TRUE) {
+            echo "persona";
+          } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+          }
+    }
+
+} else {
+  echo "<script>alert('No hay registros');</script>";
+}
+
+$conn->close();
+
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+header("Location: ../src/Grupo04.html");
+
+die();
+
+?>
--- php/reservacion.php
+++ php/reservacion.php
@@ -1,14 +1,7 @@
 <?php
-    
-// database connection code
-include 'connection.php';
-$conn = OpenCon();
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+session_start();
 
-
-// get the post records
+$txtEmail = $_POST['txtEmail'];
 $txtNombre = $_POST['txtNombre'];
 $txtApellido = $_POST['txtApellido'];
 $txtEmail = $_POST['txtEmail'];
@@ -17,87 +10,25 @@ $numCedula = $_POST['numCedula'];
 $numPlaca = $_POST['numPlaca'];
 $txtMarca = $_POST['txtMarca'];
 $txtColor = $_POST['txtColor'];
-$numEspacio = $_POST['numEspacio'];
 $Fecha = $_POST['Fecha'];
 $Hora = $_POST['Hora'];
 
+$_SESSION['txtEmail'] = $txtEmail;
+$_SESSION['txtNombre'] = $txtNombre;
+$_SESSION['txtApellido'] = $txtApellido;
+$_SESSION['txtEmail'] = $txtEmail;
+$_SESSION['numTelefono'] = $numTelefono;
+$_SESSION['numCedula'] = $numCedula;
+$_SESSION['numPlaca'] = $numPlaca;
+$_SESSION['txtMarca'] = $txtMarca;
+$_SESSION['txtColor'] = $txtColor;
+$_SESSION['Fecha'] = $Fecha;
+$_SESSION['Hora'] = $Hora;
 
-// check si existe ya el usuario
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-$foundID = false;
-$foundplaca = false;
-
-
-
-$sqlinsert1 = "INSERT INTO `dbo.persona` (ID_Persona, Nombre, Correo, Telefono) VALUES ('$numCedula', '$txtNombre', '$txtEmail', '$numTelefono')";
-$sqlinsert3 = "INSERT INTO `dbo.vehiculo` (Placa, Marca, ID_Persona, Color) VALUES ('$numPlaca', '$txtMarca', '$numCedula', '$txtColor')";
-$sqlinsert4 = "INSERT INTO `dbo.reservacion` (ID_Persona, Placa, Espacio, Fecha, Hora) VALUES ('$numCedula', '$numPlaca', '$numEspacio', '$Fecha', '$Hora')";
-
-$sqlquery = "SELECT ID_Persona, Nombre FROM `dbo.persona`";
-
-$result = mysqli_query($conn, $sqlquery);
-
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-
-    while($row = mysqli_fetch_assoc($result)) {
-        if ($row["ID_Persona"] == $numCedula && $row["Nombre"] == $txtNombre) {
-
-            //usuario existente
-            $foundID = true;
-
-            echo '<script>alert("usuario existente");</script>';
-
-            //check si existe la placa
-            $sqlquery2 = "SELECT Placa, ID_Persona FROM `dbo.vehiculo` WHERE ID_Persona = '$numCedula'";
-            $result2 = mysqli_query($conn, $sqlquery2);
-
-            while($row2 = mysqli_fetch_assoc($result2)) {
-                
-                if ($row2["Placa"] == $numPlaca) {
-                    //placa existente, insertar reservacion y parqueo
-                    $foundplaca = true;
-
-                    mysqli_query($conn, $sqlinsert4);
-    
-                    echo '<script>alert("persona y placa existentes");</script>';
-                    
-                }
-            }
-
-            if ($foundplaca == false) {
-                //placa no existente, insertar vehiculo, reservacion y parqueo
-                mysqli_query($conn, $sqlinsert3);
-                mysqli_query($conn, $sqlinsert4);
-
-                echo '<script>alert("persona existente, placa no existente");</script>';
-            }
-        }
-    }
-    if ($foundID == false) {
-        //Usuario no existente, insertar persona, vehiculo, reservacion y parqueo
-        if ($conn->query($sqlinsert1) === TRUE) {
-            echo "reservation";
-          } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-          }
-          if ($conn->query($sqlinsert3) === TRUE) {
-            echo "vehiculo";
-          } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-          }
-          if ($conn->query($sqlinsert4) === TRUE) {
-            echo "persona";
-          } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-          }
-
-          echo '<script>alert("persona no existente");</script>';
-    }
-
-} else {
-    echo '<script>alert("0 resultados");</script>';
-}
-
-$conn->close();
-?>
+header("Location: ../src/Registro.html");
+die();
+?>    
